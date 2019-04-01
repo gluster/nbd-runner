8ffdc8404b3af240011e8593abf281c509a2fd55
Xiubo Li <xiubli@redhat.com>
2019-04-01 16:52:44 +0800

    Add some comments to make it more readable
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- daemon/nbd-common.h
+++ daemon/nbd-common.h
@@ -41,11 +41,27 @@ struct nbd_device {
 
     dev_status_t status;
 
+    /*
+     * The uniqe key string for each backstore
+     *
+     * For example for Gluster backstores, it will be
+     * "volume/filepath"
+     */
     char bstore[NBD_CFGS_MAX];
-    char nbd[NBD_DLEN_MAX]; /* e.g. "/dev/nbd14" */
-    char time[NBD_TLEN_MAX]; /* e.g. "2019-02-12 12:00:37" */
 
-    void *priv; /* private ptr for handler module */
+    /* e.g. "/dev/nbd14" */
+    char nbd[NBD_DLEN_MAX];
+
+    /* The mapped time, e.g. "2019-02-12 12:00:37" */
+    char time[NBD_TLEN_MAX];
+
+    /*
+     * Private date pointer for each device
+     *
+     * This will also host the options parsed from
+     * handler->cfg_parse() if there has.
+     * */
+    void *priv;
 };
 
 struct nbd_handler {
@@ -55,13 +71,64 @@ struct nbd_handler {
 
     void *data;		/* Handler private data. */
 
+    /*
+     * Parse the handler's private options and save
+     * it into its own dev->priv data
+     */
     bool (*cfg_parse)(struct nbd_device *, const char *, nbd_response *);
+
+    /*
+     * Create the backstores.
+     *
+     * You can create the backstores by using the nbd-cli
+     * tool, if you want to support the nbd-cli create option,
+     * then you need add this support.
+     *
+     * NOTE:
+     * Or you must use the utils privided by the backstore, and
+     * when mapping the backstores, the map core code will add
+     * the backstore info into the cache and backup json file.
+     */
     bool (*create)(struct nbd_device *, nbd_response *);
+
+    /*
+     * Delete the backstores.
+     *
+     * You can delete the backstores by using the nbd-cli
+     * tool, then you need add this support.
+     *
+     * NOTE:
+     * Or you must use the utils privided by the backstore, and
+     * the delete option in the core code will only delete the
+     * backstore info from the cache and backup json file.
+     */
     bool (*delete)(struct nbd_device *, nbd_response *);
+
+    /*
+     * Map the backstore storage to the NBD device.
+     *
+     * NOTE:
+     * If the backstore is not created by using the 'nbd-cli create'
+     * then the map core code will add the backstore info into
+     * the cache and backup json file.
+     */
     bool (*map)(struct nbd_device *, nbd_response *);
+
+    /* Unmap the backstore storage from the NBD device. */
     bool (*unmap)(struct nbd_device *);
+
+    /* Get the backstore size */
     ssize_t (*get_size)(struct nbd_device *, nbd_response *);
+
+    /*
+     * Get the optiomal block size from the handler,
+     * if no please return 0.
+     */
     ssize_t (*get_blksize)(struct nbd_device *, nbd_response *);
+
+    /*
+     * Handle the IO request from the NBD device socket.
+     */
     void (*handle_request)(gpointer, gpointer);
 };
 
